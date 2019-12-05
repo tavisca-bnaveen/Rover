2ef6156e3b06d29c307ba0ca81ff97cc9c216855
tavisca-bnaveen <51917848+tavisca-bnaveen@users.noreply.github.com>
2019-12-05 23:12:08 +0530

    parser added

--- Rover.Tests/LocationTests.cs
+++ Rover.Tests/LocationTests.cs
@@ -10,55 +10,52 @@ namespace Rover.Tests
         [Fact]
         public void Test_For_Final_Location()
         {
-            var position = new Position();
-            position.X = 0;
-            position.Y = 0;
-            position.direction = DirectionFactory.GetEnumDirection("N");
+            var parser = new Parser();
+            var position = parser.InputParser("(0,0,N)");
             var location = new Location();
             var newPosition = location.GetFinalDirection(position, "MMM");
             Assert.Equal(0, newPosition.X);
             Assert.Equal(3, newPosition.Y);
             Assert.Equal('N', newPosition.direction.ToString()[0]);
+            Assert.Equal("(0,3,N)", parser.OutputParser(newPosition));
         }
         [Fact]
         public void Test_For_Final_Location2()
         {
-            var position = new Position();
-            position.X = 0;
-            position.Y = 1;
-            position.direction = DirectionFactory.GetEnumDirection("E");
+            var parser = new Parser();
+            var position = parser.InputParser("(0,1,E)");
             var location = new Location();
             var newPosition = location.GetFinalDirection(position, "MML");
             Assert.Equal(2, newPosition.X);
             Assert.Equal(1, newPosition.Y);
             Assert.Equal('N', newPosition.direction.ToString()[0]);
+            Assert.Equal("(2,1,N)", parser.OutputParser(newPosition));
         }
         [Fact]
         public void Test_For_Final_Location3()
         {
-            var position = new Position();
-            position.X = 10;
-            position.Y = 5;
-            position.direction = DirectionFactory.GetEnumDirection("N");
+            var parser = new Parser();
+            var position = parser.InputParser("(10,5,N)");
             var location = new Location();
             var newPosition = location.GetFinalDirection(position, "MMLMRMML");
             Assert.Equal(9, newPosition.X);
             Assert.Equal(9, newPosition.Y);
             Assert.Equal('W', newPosition.direction.ToString()[0]);
+            Assert.Equal("(9,9,W)", parser.OutputParser(newPosition));
+
         }
 
         [Fact]
         public void Test_For_Final_Location4()
         {
-            var position = new Position();
-            position.X = 0;
-            position.Y = 0;
-            position.direction = DirectionFactory.GetEnumDirection("N");
+            var parser = new Parser();
+            var position = parser.InputParser("(0,0,N)");
             var location = new Location();
             var newPosition = location.GetFinalDirection(position, "MMRMRMRMMLMLM");
             Assert.Equal(0, newPosition.X);
             Assert.Equal(0, newPosition.Y);
             Assert.Equal('E', newPosition.direction.ToString()[0]);
+            Assert.Equal("(0,0,E)", parser.OutputParser(newPosition));
         }
     }
 }
---
+++ Rover/Parser.cs
@@ -0,0 +1,30 @@
+﻿namespace Rover
+{
+    public class Parser
+    {
+        private Position _position;
+        public Parser()
+        {
+            _position = new Position(); 
+        }
+        public Position InputParser(string input)
+        {
+            var parsedString = input.Split(',');
+            _position.X =int.Parse( parsedString[0].Substring(1));
+            _position.Y = int.Parse(parsedString[1]);
+            _position.direction = DirectionFactory.GetEnumDirection(parsedString[2][0].ToString());
+            return _position;
+        }
+        public string OutputParser(Position position)
+        {
+            string output = "(";
+            output += position.X.ToString();
+            output += ",";
+            output += position.Y.ToString();
+            output += ",";
+            output += position.direction.ToString()[0];
+            output += ")";
+            return output;
+        }
+    }
+}
